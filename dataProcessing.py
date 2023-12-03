b34db3f8d90a1dc30993ff58b5ab6d2ed89b653f
Mihir Singh <mihirsingh@mihirs-mbp-2.middlebury.edu>
2023-12-03 14:34:57 -0500

    initial work on reading data and adding test file

---
+++ dataProcessing.py
@@ -0,0 +1,10 @@
+# Mihir Singh and Jake Gilbert
+# Handles Data Processing from Kaggle
+
+# imports
+import pandas as pd
+
+# the data processing class will be an object we use to handle data in a main file
+class dataProcessing:
+    def importData():
+        y = 4
