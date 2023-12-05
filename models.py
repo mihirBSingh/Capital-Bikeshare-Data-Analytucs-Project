6c585d0feeb51586a55bc9c540d2a4bdd40c67c1
Mihir Singh <mihirsingh@mihirs-mbp-2.middlebury.edu>
2023-12-04 19:28:50 -0500

    NN added

--- models.py
+++ models.py
@@ -1,14 +1,20 @@
 # Mihir Singh and Jake Gilbert
 # Creates AI models on dataset
 
-#imports
+# imported modules
 import dataProcessing
 import numpy as np
+import graphviz
+import calplot
+import matplotlib.pyplot as plt
+
+# imports for models
 from sklearn import tree 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
 
-# TODO: need to add count to test data from the other kaggle dataset
+# TODO: need to get plot of decision tree to show up
 
 class Models:
     def __init__(self, trainData, testData):
@@ -50,71 +56,90 @@ class Models:
         
         
     def decisionTree(self):
-        # separate training dataframe into target and features lists for training
-        targetTrain = self.dfTrain["count"]
-        featuresTrain = self.dfTrain.drop("count", axis=1)
+        # grab formatted dataframes
+        dataframes = self.formatData()
+        targetTrain = dataframes[0]
+        featuresTrain = dataframes[1]
+        targetTest = dataframes[2]
+        featuresTest = dataframes[3]
         
         # create and train tree
-        classifierTree = tree.DecisionTreeClassifier()
+        classifierTree = tree.DecisionTreeClassifier(max_depth= 5, ccp_alpha=0.005 )
         classifierTree = classifierTree.fit(featuresTrain, targetTrain)
-        
-        # separate testing dataframe into target and features lists for testing
-        targetTest = self.dfTest["cnt"]
-        featuresTest = self.dfTest.drop("cnt", axis=1)
-                
-        # plot and predict
-        tree.plot_tree(classifierTree)
+               
+        # plot
+        plotExport = tree.export_graphviz(classifierTree, out_file=None)
+        graph = graphviz.Source(plotExport)
+        graph.render("decisionTreePlot")
+         
+        # predict
         prediction = classifierTree.predict(featuresTest)
+        return prediction
         
     def randomForest(self):
-        # separate training dataframe into target and features lists for training
-        targetTrain = self.dfTrain["count"]
-        featuresTrain = self.dfTrain.drop("count", axis=1)
+        # grab formatted dataframes
+        dataframes = self.formatData()
+        targetTrain = dataframes[0]
+        featuresTrain = dataframes[1]
+        targetTest = dataframes[2]
+        featuresTest = dataframes[3]
         
         # create and train random forest classifier
         classifierRF = RandomForestClassifier(random_state=30)
-        classifierRF = classifierRF.fit(targetTrain, featuresTrain)
-        
-        # separate testing dataframe into target and features lists for testing
-        targetTest = self.dfTest["cnt"]
-        featuresTest = self.dfTest.drop("cnt", axis=1)
+        classifierRF = classifierRF.fit(featuresTrain, targetTrain)
         
         # predict
         prediction = classifierRF.predict(featuresTest)
+        return prediction
         
     def naiveBayes(self):
-        # separate training dataframe into target and features lists for training
-        targetTrain = self.dfTrain["count"]
+        # grab formatted dataframes
+        dataframes = self.formatData()
+        targetTrain = dataframes[0]
+        featuresTrain = dataframes[1]
+        targetTest = dataframes[2]
+        featuresTest = dataframes[3]
         
-        featuresTrain = self.dfTrain.drop("count", axis=1)
-        featuresTrain= featuresTrain.drop("datetime", axis=1)
-        featuresTrain= featuresTrain.drop("casual", axis=1)
-        featuresTrain= featuresTrain.drop("registered", axis=1)
-
         # create and train naive bayes classifier
         classifierNB = GaussianNB()
         classifierNB = classifierNB.fit(featuresTrain, targetTrain)
-        
-        # separate testing dataframe into target and features lists for testing
-        targetTest = self.dfTest["count"]
-        
-        featuresTest = self.dfTest.drop("count", axis=1)
-        featuresTest = featuresTest.drop("datetime", axis=1)
-        
-        # printing for debugging
-        print(featuresTrain)
-        print(featuresTest)
-        print(targetTrain)
 
         # predict
         prediction = classifierNB.predict(featuresTest)
         return prediction
     
+    def neuralNet(self):
+        # grab formatted dataframes
+        dataframes = self.formatData()
+        targetTrain = dataframes[0]
+        featuresTrain = dataframes[1]
+        targetTest = dataframes[2]
+        featuresTest = dataframes[3]
+        
+        # create and train neural network
+        classifierNN = MLPClassifier(solver='sgd', max_iter=2000)
+        classifierNN = classifierNN.fit(featuresTrain, targetTrain)
+        
+        # predict
+        prediction = classifierNN.predict(featuresTest)
+        return prediction
+
+    
+    def runModels():
+        print("Choose which model you want to run")
+
+# create models
 model = Models("train.csv","test.csv")    
 print("model ")
-myarray = model.naiveBayes()
-print(myarray[1000:1500])
-        
+# myarray = model.randomForest() # Random Forest
+# myarray = model.decisionTree() # Decision Tree
+myarray = model.naiveBayes()   # Naive Bayes
+print(len(myarray))
+# myarray = model.neuralNet() # Neural Net
+
+# make year plot of business
+
+
 
         
         
